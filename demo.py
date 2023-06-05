9cff2a4c1fff5842c2e0edd139c6841a4ed6c23c
homaiminhnhat0101 <homaiminhnhat.work@gmail.com>
2023-06-05 17:12:08 +0700

    try

---
+++ demo.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np 
+
+path = "C:/Users/PhiLong Technology/OneDrive/Desktop/DA//Data/Tradin/Stock_FMCG/Hangtieudung.csv"
+df = pd.read_csv(path)
+
+df.head(5)
+
+
+
