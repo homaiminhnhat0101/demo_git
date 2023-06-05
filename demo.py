d54d3b316a8e7e6d1e9955ac30bd3b4abc30511e
homaiminhnhat0101 <homaiminhnhat.work@gmail.com>
2023-06-05 17:20:05 +0700

    try again

--- demo.py
+++ demo.py
@@ -5,6 +5,12 @@ path = "C:/Users/PhiLong Technology/OneDrive/Desktop/DA//Data/Tradin/Stock_FMCG/
 df = pd.read_csv(path)
 
 df.head(5)
+type(df.snapshot_date)
+
+df.snapshot_date = pd.to_datetime(df.snapshot_date)
+df.snapshot_date.dt.month
+
+print('successful')
 
 
 
